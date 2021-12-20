168f425343613ebf4c45325dffbb1867b20035d2
emanuelemaffeo <emanuele.maffeo@agilelab.iut>
2021-12-20 13:43:52 +0100

    readDarwinAvro reads all messages based on a conf and populate output record with deserialized avro message maps

--- morphlines/src/main/java/it/agilelab/darwin/morphline/ReadDarwinAvroBuilder.java
+++ morphlines/src/main/java/it/agilelab/darwin/morphline/ReadDarwinAvroBuilder.java
@@ -20,7 +20,6 @@ import com.typesafe.config.ConfigFactory;
 import it.agilelab.darwin.manager.AvroSchemaManager;
 import it.agilelab.darwin.manager.AvroSchemaManagerFactory;
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericContainer;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryDecoder;
@@ -36,12 +35,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public final class ReadDarwinAvroBuilder implements CommandBuilder {
@@ -50,8 +47,9 @@ public final class ReadDarwinAvroBuilder implements CommandBuilder {
      * The MIME type identifier that will be filled into output records
      */
     public static final String AVRO_MEMORY_MIME_TYPE = "avro/java+memory";
+    public static final String MESSAGE_FIELDS = "messageFields";
+    public static final String DARWIN_CONF = "darwinConf";
 
-    protected final Logger LOG = LoggerFactory.getLogger(getClass());
 
 
     @Override
@@ -65,24 +63,26 @@ public final class ReadDarwinAvroBuilder implements CommandBuilder {
     }
 
 
-    final static class ReadDarwinAvro extends AbstractParser {
+    final class ReadDarwinAvro extends AbstractParser {
 
         private BinaryDecoder binaryDecoder = null;
 
         private final AvroSchemaManager schemaManager;
-
+        private final List<String> messageFields;
 
         public ReadDarwinAvro(CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context) {
             super(builder, config, parent, child, context);
-            String darwinConfiguration = getConfigs().getString(config, "darwinConf");
+            Config darwinConfiguration = getConfigs().getConfig(config, DARWIN_CONF);
 
             if (darwinConfiguration != null && !darwinConfiguration.isEmpty()) {
                 this.schemaManager = AvroSchemaManagerFactory
-                        .initialize(ConfigFactory.parseString(darwinConfiguration)
-                                .getConfig("darwin"));
+                        .initialize(darwinConfiguration);
             } else {
                 throw new IllegalArgumentException("Darwin configuration is empty");
             }
+
+            messageFields = getConfigs()
+                    .getStringList(config, MESSAGE_FIELDS, Collections.singletonList(Fields.ATTACHMENT_BODY));
         }
 
         @Override
@@ -91,9 +91,7 @@ public final class ReadDarwinAvroBuilder implements CommandBuilder {
 
             template.put(Fields.ATTACHMENT_MIME_TYPE, ReadDarwinAvroBuilder.AVRO_MEMORY_MIME_TYPE);
 
-            List<Object> attachments = template.get(Fields.ATTACHMENT_BODY);
-
-            Map<String, Object> avroMap = decodeAvro(attachments);
+            Map<String, Map<String, Object>> avroMap = decodeMessages(template);
 
             removeAttachments(template);
 
@@ -107,40 +105,34 @@ public final class ReadDarwinAvroBuilder implements CommandBuilder {
             return getChild().process(outputRecord);
         }
 
-        private Map<String, Object> decodeAvro(List<Object> attachments) {
-            List<InputStream> inputStreams = attachments.stream()
-                    .map(x -> new ByteArrayInputStream((byte[]) x))
-                    .collect(Collectors.toList());
-            List<Map<String, Object>> allAvroMaps = inputStreams.stream().map(in -> {
-                Decoder decoder = DecoderFactory.get().binaryDecoder(in, binaryDecoder);
-                Schema schema = schemaManager.extractSchema(in).right().get();
-                GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
-                Map<String, Object> recordMap = new HashMap<>();
-                try {
-                    GenericRecord record = datumReader.read(null, decoder);
-                    record.getSchema().getFields().forEach(x -> recordMap.put(x.name(),
-                            Optional.of(record.get(x.name())).map(Object::toString).orElse(null)));
-                    return recordMap;
-                } catch (IOException e) {
-                    LOG.error("Unable to decode avro message: ", e);
-                    return recordMap;
+        private Map<String, Map<String, Object>> decodeMessages(Record morphlineRecord) {
+            Map<String, InputStream> avroMap = new HashMap<>();
+            messageFields.forEach(f -> {
+                Object value = morphlineRecord.getFirstValue(f);
+                if (value != null) {
+                    avroMap.put(f, new ByteArrayInputStream((byte[]) value));
                 }
-            }).collect(Collectors.toList());
+            });
 
-            Map<String, Object> result = new HashMap<>();
-
-            allAvroMaps.forEach(result::putAll);
-
-            return result;
+            return avroMap.entrySet().stream()
+                    .map(el -> new AbstractMap.SimpleEntry<>(el.getKey(), decodeAvro(el.getValue())))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         }
 
-        private boolean extract(GenericContainer datum, Record inputRecord) {
-            incrementNumRecords();
-            Record outputRecord = inputRecord.copy();
-            outputRecord.put(Fields.ATTACHMENT_BODY, datum);
-
-            // pass record to next command in chain:
-            return getChild().process(outputRecord);
+        private Map<String, Object> decodeAvro(InputStream in) {
+            Decoder decoder = DecoderFactory.get().binaryDecoder(in, binaryDecoder);
+            Schema schema = schemaManager.extractSchema(in).right().get();
+            GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+            Map<String, Object> recordMap = new HashMap<>();
+            try {
+                GenericRecord record = datumReader.read(null, decoder);
+                record.getSchema().getFields().forEach(x -> recordMap.put(x.name(),
+                        Optional.ofNullable(record.get(x.name())).map(Object::toString).orElse(null)));
+                return recordMap;
+            } catch (IOException e) {
+                LOG.error("Unable to decode avro message: ", e);
+                return recordMap;
+            }
         }
 
     }
